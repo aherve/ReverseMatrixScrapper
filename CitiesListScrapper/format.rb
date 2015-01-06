0e54df5d6ae90bae62e774bb52beaa91ff4545e9
Aurélien Hervé <mail@aurelien-herve.com>
2015-01-06 15:01:27 +0100

    ok

--- CitiesListScrapper/format.rb
+++ CitiesListScrapper/format.rb
@@ -23,7 +23,7 @@ csv_head = [
   :unknown,
   :unknown,
   :unknown,
-  :unknown,
+  :population,
   :unknown,
   :lng,
   :lat,
@@ -35,12 +35,13 @@ head = [
   :readable_name,
   :lat,
   :lng,
+  :population,
 ]
 
 # reformatting csv
-cities = CSV::read('./data/villes_france.csv').map{|a| csv_head.zip(a)}.map(&:to_h).map{|h| h.select{|k,v| head.include? k}}
+cities = CSV::read('./data_raw/villes_france.csv').map{|a| csv_head.zip(a)}.map(&:to_h).map{|h| h.select{|k,v| head.include? k}}
 
-File.open('./data/formatted_cities.csv', 'w') do |f|
+File.open('./formatted_cities.csv', 'w') do |f|
   f.puts head.join("\t")
   cities.each do |city|
     f.puts head.map{|k| city[k]}.join("\t")
